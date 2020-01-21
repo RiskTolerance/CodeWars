1a8ffac46c8b08cf06fda232cc2ac4fe3a3fe222
melvin.code <melvingonzalez470@gmail.com>
2020-01-21 14:28:36 -0500

    sup

---
+++ SumOfPositive.js
@@ -0,0 +1,5 @@
+function sumTwoSmallestNumbers(numbers) {  
+ numbers = numbers.sort(function(a, b){return a - b; });
+  return numbers[0] + numbers[1];
+}
+//what up this is the right code instead of blank
