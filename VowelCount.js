da101f2df337d26956b6f3d0da80db7530acf414
RiskTolerance <isaac.druin@gmail.com>
2020-01-27 10:17:18 -0800

    daily morning kata

---
+++ VowelCount.js
@@ -0,0 +1,9 @@
+//kata link
+//https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/javascript
+
+function getCount(str) {
+    let v = str.match(/[aeiou]/ig);
+    if(v === null){return 0} else {return v.length};
+  }
+
+  //I should learn Regex OuO
